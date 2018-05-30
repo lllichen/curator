2dbde3ff142b5768e48b7987191054d0d9cb5031
lichen <lichen@daojia.com>
2018-05-30 17:43:44 +0800

    add test code

--- curator-examples/src/main/java/exercise/leader/ExampleClient.java
+++ curator-examples/src/main/java/exercise/leader/ExampleClient.java
@@ -6,6 +6,7 @@ import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -29,16 +30,39 @@ public class ExampleClient extends LeaderSelectorListenerAdapter implements Clos
         this.leaderSelector = new LeaderSelector(client,path ,this);
 
         //for most cases you will want your instance to requeue when it relinquishes leadership
-//        leaderSelector.autoRequeue();
+        leaderSelector.autoRequeue();
+    }
+
+    public void start(){
+        // the selection for this instance doesn't start until the leader selector is started
+        // leader selection is done in the background so this call to leaderSelector.start() returns immediately
+        leaderSelector.start();
     }
 
     @Override
     public void close() throws IOException {
+        leaderSelector.close();
 
     }
 
     @Override
     public void takeLeadership(CuratorFramework client) throws Exception {
+
+        // we are now the leader. This method should not return until we want to relinquish leadership
+        final int  waitSeconds = (int)((5 * Math.random())+1);
+
+        System.out.println(name + " is now the leader.Waiting "+ waitSeconds + " seconds...");
+        System.out.println(name + " has been leader " + leaderCount.getAndIncrement() + " time(s) before.");
+        try {
+            Thread.sleep( TimeUnit.SECONDS.toMillis( waitSeconds ) );
+        }catch (InterruptedException e){
+            System.err.println( name + " was interrupted." );
+            Thread.currentThread().interrupt();
+        }
+        finally {
+            System.out.println(name + " relinquishing leadership.\n");
+        }
+
 
     }
 }
